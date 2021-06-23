5587e3db4a858c85839b637a28a6a7bc1101e754
Daniel Farias <dan.tfarias@gmail.com>
2021-06-23 15:32:22 -0300

    update

---
+++ lib/wabanex_web/schema.ex
@@ -0,0 +1,14 @@
+defmodule WabanexWeb.Schema do
+  use Absinthe.Schema
+
+  import_types WabanexWeb.Schema.Types.Root
+
+  query do
+    import_fields :root_query
+  end
+
+  mutation do
+    import_fields :root_mutation
+  end
+
+end
